502871a6219e5be1a92a08c4ead66f09abd68a33
Shin Ji Hwan <jumprope123@naver.com>
2023-03-16 22:30:33 +0900

    임시커밋

--- styles/01/emotion.js
+++ styles/01/emotion.js
@@ -1,158 +1,157 @@
 import styled from "@emotion/styled";
-import { style } from "@mui/system";
 
 export const WrapperDiv = styled.div`
-    width : 640px;
-    height : 1138px;
-    border : 1px solid black;
+    width: 640px;
+    height: 1138px;
+    border: 1px solid black;
     margin: 100px;
-`
+`;
 
 export const IconWrapper = styled.div`
-    width : 100%;
-    padding-top : 79px;
-    display : flex;
-    flex-direction: row;
-    justify-content: flex-end;
-    padding-right : 56px;
-`
-
-export const TitleWrapper = styled.div`
-    width : 100%;  
+    width: 100%;
+    padding-top: 79px;
     display: flex;
     flex-direction: row;
-    padding-right : 56px;
+    justify-content: flex-end;
+    padding-right: 56px;
+`;
+
+export const TitleWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    padding-right: 56px;
     justify-content: space-between;
     padding-left: 49px;
-    padding-top:40px;
-`
+    padding-top: 40px;
+`;
 
 export const TitleDiv = styled.div`
     width: 212px;
     height: 64px;
     font-weight: 700;
     font-size: 40px;
-`
+`;
 
 export const IconDiv = styled.div`
     display: flex;
-    flex-direction: row; 
+    flex-direction: row;
     justify-content: flex-end;
-`
+`;
 
 export const ImageIcon = styled.img`
     width: 60px;
     height: 60px;
-`
+`;
 
 export const UserName = styled.div`
-    padding-left :19px;
+    padding-left: 19px;
     font-weight: 700;
     font-size: 24px;
     display: flex;
     align-items: center;
-`
+`;
 export const RightArrow = styled.div`
     display: flex;
     align-items: center;
     color: gray;
-`
+`;
 
 export const MenuWrapper = styled.div`
-    display:flex;
+    display: flex;
     flex-direction: row;
     justify-content: flex-between;
-    margin-left:49px;
-`
+    margin-left: 49px;
+`;
 
 export const MenuItem = styled.div`
-    margin-top:39px; 
+    margin-top: 39px;
     font-weight: 700;
     font-size: 30px;
-    color: #CACACA;
-    margin-right:52px;
-`
+    color: #cacaca;
+    margin-right: 52px;
+`;
 
 export const ActivatedMenuItem = styled.div`
-    margin-top:39px; 
+    margin-top: 39px;
     font-weight: 700;
     font-size: 30px;
-    color: #FF1B6D;
-    margin-right:52px;
-    border-bottom: 2px solid #FF1B6D;
-`
+    color: #ff1b6d;
+    margin-right: 52px;
+    border-bottom: 2px solid #ff1b6d;
+`;
 
 export const UnderLine = styled.div`
     margin-top: 58px;
-    border-bottom: 1px solid #CACACA;
-`
+    border-bottom: 1px solid #cacaca;
+`;
 
 export const ContentsWrapper = styled.div`
     padding-left: 50px;
     padding-right: 40px;
-`
+`;
 
 export const FirstContentsDiv = styled.div`
-    display:flex;
-    width:100%;
-    flex-direction:row;
+    display: flex;
+    width: 100%;
+    flex-direction: row;
     justify-content: space-between;
-    margin-top:25px;
-`
+    margin-top: 25px;
+`;
 
 export const ContentsDiv = styled.div`
-    display:flex;
-    width:100%;
-    flex-direction:row;
+    display: flex;
+    width: 100%;
+    flex-direction: row;
     justify-content: space-between;
-    margin-top:44px;
-`
+    margin-top: 44px;
+`;
 
 export const LeftContents = styled.div`
-    display:flex;
+    display: flex;
     flex-direction: column;
     justify-content: center;
-`
+`;
 export const LeftTopContents = styled.div`
     font-size: 18px;
-    color: #ADADAD;
-`
+    color: #adadad;
+`;
 
 export const LeftBottomContents = styled.div`
-    margin-top:5px;
+    margin-top: 5px;
     font-weight: 400;
     font-size: 24px;
     color: #000000;
-`
+`;
 
 export const RightContents = styled.div`
-    display:flex;
+    display: flex;
     flex-direction: column;
     justify-content: center;
-`
+`;
 
 export const UnderMenuBar = styled.div`
-    display:flex;
+    display: flex;
     justify-content: space-around;
-    padding-top:14px;
-`
+    padding-top: 14px;
+`;
 
 export const UnderMenuDiv = styled.div`
-    display:flex;
+    display: flex;
     flex-direction: column;
     justify-content: center;
-`
+`;
 
 export const IconCenter = styled.div`
-    display:flex;
+    display: flex;
     flex-direction: row;
     justify-content: center;
-`
+`;
 
 export const UnderMenuDivText = styled.div`
-    display:flex;
+    display: flex;
     flex-direction: row;
     justify-content: center;
-    color: #ADADAD;
+    color: #adadad;
     font-size: 16px;
-`
+`;
